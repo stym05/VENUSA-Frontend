ec3b892e5186ccf235e46b28175595f4ca60969e
Aabhishek29 <starkabhishek29@gmail.com>
2025-08-10 19:42:42 +0530

    minor updates

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,9 +2,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { URL } from "./urls";
 import Store from "../store";
 
-// export const DOMAIN = "https://webservices.venusa.co.in/";
+export const DOMAIN = "https://webservices.venusa.co.in";
 
-export const DOMAIN = "http://localhost:8000";
+// export const DOMAIN = "http://localhost:8000";
 
 
 
